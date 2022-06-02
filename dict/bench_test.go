f927e1899e361042d082da8b84e42cb4a24256fb
Jiaqi_Shen <gleiphir2769@gmail.com>
2022-06-02 23:00:17 +0800

    test: Add test for sync.map

--- dict/bench_test.go
+++ dict/bench_test.go
@@ -3,21 +3,23 @@ package dict
 import (
 	"math/rand"
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
 
 func BenchmarkSimple(b *testing.B) {
 	d := MakeSimpleDict()
-
+	rand.Seed(time.Now().Unix())
 	b.SetParallelism(10)
+	var exist bool
 	b.RunParallel(func(pb *testing.PB) {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
 		for pb.Next() {
-			key := strconv.Itoa(r.Intn(1000000))
-			d.Put(key, 0)
-			d.Get(key)
+			key := strconv.Itoa(r.Intn(100000000000))
+			if _, exist = d.Get(key); !exist {
+				d.Put(key, key)
+			}
 		}
 	})
 }
@@ -27,17 +29,40 @@ func BenchmarkSimpleMoreRead(b *testing.B) {
 	d := MakeSimpleDict()
 	rand.Seed(time.Now().Unix())
 	b.SetParallelism(10)
+	var exist bool
 	b.RunParallel(func(pb *testing.PB) {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		lastKey := ""
 		for pb.Next() {
-			key := strconv.Itoa(r.Intn(1000000))
+			key := strconv.Itoa(r.Intn(100000000000))
 			p := rand.Intn(10)
 			if p == 0 {
-				d.Put(key, 0)
-				lastKey = key
+				if _, exist = d.Get(key); !exist {
+					d.Put(key, key)
+				}
 			} else {
-				d.Get(lastKey)
+				d.Get(key)
+			}
+		}
+	})
+}
+
+// BenchmarkSimpleMoreRead set read:write as 9:1
+func BenchmarkSimpleMoreWrite(b *testing.B) {
+	d := MakeSimpleDict()
+	rand.Seed(time.Now().Unix())
+	b.SetParallelism(10)
+	var exist bool
+	b.RunParallel(func(pb *testing.PB) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for pb.Next() {
+			key := strconv.Itoa(r.Intn(100000000000))
+			p := rand.Intn(10)
+			if p != 0 {
+				if _, exist = d.Get(key); !exist {
+					d.Put(key, key)
+				}
+			} else {
+				d.Get(key)
 			}
 		}
 	})
@@ -45,15 +70,16 @@ func BenchmarkSimpleMoreRead(b *testing.B) {
 
 func BenchmarkShard(b *testing.B) {
 	d := MakeShardDict(16)
-
+	rand.Seed(time.Now().Unix())
 	b.SetParallelism(10)
+	var exist bool
 	b.RunParallel(func(pb *testing.PB) {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
 		for pb.Next() {
-			key := strconv.Itoa(r.Intn(1000000))
-			d.Put(key, 0)
-			d.Get(key)
+			key := strconv.Itoa(r.Intn(100000000000))
+			if _, exist = d.Get(key); !exist {
+				d.Put(key, key)
+			}
 		}
 	})
 }
@@ -63,17 +89,89 @@ func BenchmarkShardMoreRead(b *testing.B) {
 	d := MakeShardDict(16)
 	rand.Seed(time.Now().Unix())
 	b.SetParallelism(10)
+	var exist bool
 	b.RunParallel(func(pb *testing.PB) {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		lastKey := ""
 		for pb.Next() {
-			key := strconv.Itoa(r.Intn(1000000))
+			key := strconv.Itoa(r.Intn(100000000000))
 			p := rand.Intn(10)
 			if p == 0 {
-				d.Put(key, 0)
-				lastKey = key
+				if _, exist = d.Get(key); !exist {
+					d.Put(key, key)
+				}
 			} else {
-				d.Get(lastKey)
+				d.Get(key)
+			}
+		}
+	})
+}
+
+// BenchmarkShardMoreRead set read:write as 9:1
+func BenchmarkShardMoreWrite(b *testing.B) {
+	d := MakeShardDict(16)
+	rand.Seed(time.Now().Unix())
+	b.SetParallelism(10)
+	var exist bool
+	b.RunParallel(func(pb *testing.PB) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for pb.Next() {
+			key := strconv.Itoa(r.Intn(100000000000))
+			p := rand.Intn(10)
+			if p != 0 {
+				if _, exist = d.Get(key); !exist {
+					d.Put(key, key)
+				}
+			} else {
+				d.Get(key)
+			}
+		}
+	})
+}
+
+func BenchmarkSyncMap(b *testing.B) {
+	d := sync.Map{}
+	rand.Seed(time.Now().Unix())
+	b.SetParallelism(10)
+	b.RunParallel(func(pb *testing.PB) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for pb.Next() {
+			key := strconv.Itoa(r.Intn(100000000000))
+			d.LoadOrStore(key, key)
+		}
+	})
+}
+
+func BenchmarkSyncMapMoreRead(b *testing.B) {
+	d := sync.Map{}
+	rand.Seed(time.Now().Unix())
+	b.SetParallelism(10)
+	b.RunParallel(func(pb *testing.PB) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for pb.Next() {
+			key := strconv.Itoa(r.Intn(100000000000))
+			p := rand.Intn(10)
+			if p == 0 {
+				d.LoadOrStore(key, key)
+			} else {
+				d.Load(key)
+			}
+		}
+	})
+}
+
+func BenchmarkSyncMapMoreWrite(b *testing.B) {
+	d := sync.Map{}
+	rand.Seed(time.Now().Unix())
+	b.SetParallelism(10)
+	b.RunParallel(func(pb *testing.PB) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for pb.Next() {
+			key := strconv.Itoa(r.Intn(100000000000))
+			p := rand.Intn(10)
+			if p != 0 {
+				d.LoadOrStore(key, key)
+			} else {
+				d.Load(key)
 			}
 		}
 	})
